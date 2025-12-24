09b76f6311632680e81b8cf58c2ab3fbfd7c6c0d
MiggyL <116647018+MiggyL@users.noreply.github.com>
2025-12-24 17:36:05 +0800

    Update DeploymentSelector.js

--- app/components/DeploymentSelector.js
+++ app/components/DeploymentSelector.js
@@ -11,7 +11,7 @@ const deployments = [
   },
   {
     name: 'Netlify',
-    url: 'https://miguel-ai.netlify-2.app/',
+    url: 'https://miguel-ai-2.netlify.app/',
     color: 'bg-teal-500',
     textColor: 'text-teal-600'
   },
